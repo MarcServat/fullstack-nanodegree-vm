fc48d56478a05136b44742a652ab8ff46b92f79f
MarcServat <marcservat@hotmail.com>
2018-05-22 22:31:48 +0200

    WIP

---
+++ vagrant/catalog/models.py
@@ -0,0 +1,67 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
+import random
+import string
+from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer,
+                         BadSignature, SignatureExpired)
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    password_hash = Column(String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(secret_key, expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(80), nullable=False)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'cat_id': self.category_id,
+            'description': self.description,
+            'id': self.id,
+            'title': self.title
+        }
+
+engine = create_engine('sqlite:///category.db')
+
+Base.metadata.create_all(engine)
---
+++ vagrant/run.py
@@ -0,0 +1,86 @@
+from flask_sqlalchemy import SQLAlchemy
+from database_setup import Base, Restaurant, MenuItem
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurantmenu.db'
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def DefaultRestaurantmenu():
+    restaurant = db.session.query(Restaurant).first()
+    items = db.session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+
+
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = db.session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = db.session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    return render_template('menu.html', restaurant=restaurant, items=items)
+
+
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
+def newMenuItem(restaurant_id):
+    if request.method == 'POST':
+        newItem = MenuItem(name=request.form['name'],
+                           restaurant_id=restaurant_id)
+        db.session.add(newItem)
+        db.session.commit()
+        flash('New item has been created')
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
+
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit',
+           methods=['GET', 'POST'])
+def editMenuItem(restaurant_id, menu_id):
+    editedItem = db.session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        db.session.add(editedItem)
+        db.session.commit()
+        flash('Item has been edited')        
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('editmenuitem.html',
+                               restaurant_id=restaurant_id,
+                               menu_id=menu_id,
+                               item=editedItem)
+
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete', 
+           methods=['GET', 'POST'])
+def deleteMenuItem(restaurant_id, menu_id):
+    deleteItem = db.session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        db.session.delete(deleteItem)
+        db.session.commit()
+        flash('Item has been delete')                
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('deletemenuitem.html',
+                               restaurant_id=restaurant_id,
+                               menu_id=menu_id,
+                               item=deleteItem)
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = db.session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = db.session.query(MenuItem).filter_by(
+            restaurant_id=restaurant.id).all()
+    return jsonify(MenuItem=[i.serialize for i in items])
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def itemMenuJSON(restaurant_id, menu_id):
+    items = db.session.query(MenuItem).filter_by(
+            id=menu_id)
+    return jsonify(MenuItem=[i.serialize for i in items])
+
+if __name__ == '__main__':
+    app.secret_key = 'secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
